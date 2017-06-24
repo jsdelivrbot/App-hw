d5a0b56d72a7297482e6c092384a088d9a51828e
ayuzero <ayuzero8427@gmail.com>
2017-06-25 05:43:38 +0800

    backend push

---
+++ Handicraft/src/components/posts_index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
+import Postnew from './posts_new';
+import Header from './header';
+import Intro_1 from '../containers/intro_1';
+import Intro_2 from '../containers/intro_2';
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <div>
+            <Link to={"posts/" + post.id}>
+              <span className="pull-xs-right">{post.categories}</span>
+              <strong>{post.title}</strong>
+            </Link>
+          </div>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>       
+          <Header />
+          <Intro_1 />
+          <Intro_2 />
+          <Postnew />      
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
---
+++ Handicraft/src/containers/intro_1.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Intro_1 extends Component {
+    render(){
+        return(
+            <div className="intro1">
+                <div className="int1">
+                    <img src="http://i.imgur.com/XgVHGyT.png" />
+                    <h3>耐心</h3>
+                </div>
+                <div className="int2">
+                    <img src="http://i.imgur.com/fqykZTp.png" />
+                    <h3>創意</h3>
+                </div>
+                <div className="int3">
+                    <img src="http://i.imgur.com/DVjz6Al.png" />
+                    <h3>手作</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Intro_1;
